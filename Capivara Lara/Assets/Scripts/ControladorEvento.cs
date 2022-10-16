7b71909838ea4da1e0e4871ae6003c2f7444e0ce
Bruno Lemos <91765091+brenolumos@users.noreply.github.com>
2022-10-16 20:34:34 -0300

    Diálogos e Filtragem
    Implementados diálogos e sistema de filtragem

--- Capivara Lara/Assets/Scripts/ControladorEvento.cs
+++ Capivara Lara/Assets/Scripts/ControladorEvento.cs
@@ -7,11 +7,16 @@ public class ControladorEvento : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
+
     void OnMouseOver() {
         this.animator.SetBool("estaIdle", false);
+        ChatBubble.GetChatBuBbleScript("Clique no lixo utilizando o botão esquerdo do mouse e arraste para a lixeira correta de acordo com o material.");
+        ChatBubble.AparecerBubble();
     }
 
     void OnMouseExit() {
         this.animator.SetBool("estaIdle", true);
+        ChatBubble.GetChatBuBbleScript("");
+        ChatBubble.SumirBubble();
     }
 }
