49c8cd259d0c2a8241e9de5a17e3be4ab636c34d
Leandro Felipe Moreira <leandroofelipee@gmail.com>
2022-10-16 01:07:30 -0300

    Atualizacao Menu e nome cenas

--- Capivara Lara/Assets/ProgressoObjetivo.cs
+++ Capivara Lara/Assets/ProgressoObjetivo.cs
@@ -11,7 +11,40 @@ namespace Assets {
         }
 
         public static void SetLixosColetados() {
+
             LixosColetados++;
+            
+            if(LixosColetados > 4)
+                FalaCapivara(2);
+
+            if (LixosColetados > 17)
+                MudaClima(true);
+        }
+
+        private static void MudaClima(bool tempoBom)
+        {
+            if (tempoBom)
+            {
+
+            }
+        }
+
+        private static void FalaCapivara(int tipoFrase)
+        {
+            switch (tipoFrase)
+            {
+                case 1://Instruções
+                    
+                    break;
+                case 2://Insentivo
+                    
+                    break;
+                case 3://Lixo Errado
+                    
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
